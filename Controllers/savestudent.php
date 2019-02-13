b6b849357773dff84016b48a98072ad3432cb6a2
ahmed hessuin. <ahmed_1k997@yahoo.com>
2019-02-13 21:43:50 +0200

    Merge origin/ahmed2
    Conflicts:
    	View/student/add_event.php
    	View/student/common/head.phMerge origin/ahmed2
    
    Conflicts:
    	View/student/add_event.php
    	View/student/common/head.phpp

--- Controllers/savestudent.php
+++ Controllers/savestudent.php
@@ -1,17 +1,12 @@
 <?php
 
 include_once("../Controllers/common.php");
-include_once("../Models/student.php");
+include_once('../Models/user.php');
+include_once('../Models/studentxcourse.php');
 Database::DBConnect();
-$id = safeGet("id", 0);
-$id_user = safeGet("id_user", 0);
-if ($id == 0) {
-    Student::add(safeGet("name"));
-} else {
-    $student = new Student($id);
-    $student->name = safeGet("name");
-    $student->save();
-}
-echo $student->name;
-//header("Location: ../view/teacher/students.php?id=$id_user&&id_st=$id");
+$id_user = safeGet("id", 0);
+$crs_id = safeGet("crs_id", 0);
+$std_id=User::searchbyUserName(safeGet('name'));
+Studentxcourse::add($crs_id, $std_id, '' ,'');
+header("Location: ../view/teacher/student_management.php?id=$id_user&&crs_id=$crs_id&&$std_id");
 ?>
