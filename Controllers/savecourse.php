e2c153af61bc19dd2599d4f004e8d30eafe4bcab
Ibrahim Amr <ibrahem.amr.eid@gmail.com>
2019-02-14 00:11:17 +0200

    Add and Edit Course.

--- Controllers/savecourse.php
+++ Controllers/savecourse.php
@@ -4,19 +4,20 @@ include_once("../Controllers/common.php");
 include_once("../Models/Courses.php");
 
 Database::DBConnect();
-$id = safeGet("id", 0);
-if ($id == 0) {
-    Courses::add(safeGet("name"), safeGet("study_year"), safeGet("max_degree") , safeGet("description") , safeGet("type") , safeGet("teacher_id"));
+$user_id = safeGet("user_id", 0);
+$course_id = safeGet("course_id", 0);
+if ($course_id == 0) {
+    Courses::add(safeGet("name"), safeGet("study_year"), safeGet("max_degree"), safeGet("description"), safeGet("type"), $user_id);
 } else {
-    $courses = new Courses($id);
+    $courses = new Courses($course_id);
     $courses->name = safeGet("name");
     $courses->study_year = safeGet("study_year");
     $courses->max_degree = safeGet("max_degree");
     $courses->description = safeGet("description");
     $courses->type = safeGet("type");
-    $courses->teacher_id = safeGet("teacher_id");
-     
+    $courses->teacher_id = $user_id;
+
     $courses->save();
 }
-header('Location: ../view/teacher/courses.php');
+header('Location: ../View/teacher/courses.php?id=' . $user_id);
 ?>
--- View/teacher/editcourse.php
+++ View/teacher/editcourse.php
@@ -1,10 +1,12 @@
 <?php
 include_once("../../Controllers/common.php");
 include_once('../../Models/Courses.php');
-$id = safeGet('crsid' , 0);
+include_once('../../Models/user.php');
+$user_id = safeGet('id', 0);
+$course_id = safeGet('crs_id', 0);
 Database::DBConnect();
-$courses = new Courses($id);
-$user = new User($id); // get the user id 
+$courses = new Courses($course_id);
+$user = new User($user_id); // get the user id 
 ?>
 
 <!DOCTYPE html>
@@ -18,46 +20,47 @@ $user = new User($id); // get the user id
             <div class="container-fluid">
                 <div role="main" class="container">
 
-                    <h2 class="mt-5"><?= ($id) ? "Edit" : "Add" ?> Course</h2>
+                    <h2 class="mt-5"><?= ($course_id) ? "Edit" : "Add" ?> Course</h2>
 
-                    <form action="controllers/savecourse.php" method="post">
-                        <input type="hidden" name="id" value="<?= $courses->id ?>">
+                    <form action="../../Controllers/savecourse.php" method="post">
+                        <input type="hidden" name="user_id" value="<?= $user_id ?>">
+                        <input type="hidden" name="course_id" value="<?= $course_id ?>">
                         <div class="card"  style='background: #002752'>
                             <div class="card-body">
-                                
+
                                 <div class="form-group row gutters">
-                                    <label for="course_name" class="col-sm-2 col-form-label">Name</label>
+                                    <label for="course_name" class="col-sm-2 col-form-label"  style="color: white;">Name</label>
                                     <div class="col-sm-10" style="margin-bottom: 10px">
-                                        <input class="form-control" type="text" name="name" value="<?= $courses->get('name') ?>" required>
+                                        <input class="form-control" type="text" name="name" value="<?= $courses->name ?>" required>
                                     </div>
-                                    
+
                                     <div class="col-md-2"  style="color: white;">Privacy</div>
                                     <div class="col-md-10">
                                         <div class="form-group">
                                             <select class="form-control" name="Privacy">
-                                                <option value="1">Public</option>
-                                                <option value="0">Private</option>
+                                                <option value="1" <?= ($courses->type) ? "Selected" : "" ?>>Public</option>
+                                                <option value="0" <?= ($courses->type) ? "" : "Selected" ?>>Private</option>
                                             </select>
                                         </div>
                                     </div>
-                                    
-                                    <label  for="course_year" class="col-sm-2 col-form-label">Study Year</label>
+
+                                    <label  for="course_year" class="col-sm-2 col-form-label"  style="color: white;">Study Year</label>
                                     <div class="col-sm-10" style="margin-bottom: 10px">
-                                        <input class="form-control" type="text" name="study_year" value="<?= $courses->get('study_year') ?>" required>
+                                        <input class="form-control" type="text" name="study_year" value="<?= $courses->study_year ?>" required>
                                     </div>
-                                    <label for="course_degree" class="col-sm-2 col-form-label">Max Degree</label>
+                                    <label for="course_degree" class="col-sm-2 col-form-label" style="color: white;">Max Degree</label>
                                     <div class="col-sm-10">
-                                        <input class="form-control" type="text" name="max_degree" value="<?= $courses->get('max_degree') ?>" required>
+                                        <input class="form-control" type="text" name="max_degree" value="<?= $courses->max_degree ?>" required>
                                     </div>  
-                                    
-                                       <label for="course_description" class="col-sm-2 col-form-label" style="color: white;margin-top: 15px;">Description</label>
+
+                                    <label for="course_description" class="col-sm-2 col-form-label" style="color: white;margin-top: 15px;">Description</label>
                                     <div class="col-sm-10" style="margin-top: 15px;">
                                         <textarea class="form-control"  name="description" rows="8" cols="30" required><?= $courses->description ?></textarea>
                                     </div>  
                                 </div>
-                                
-                                 <div class="col-sm-2 float-right" style="margin-bottom: 10px">
-                                    <button class="form-control btn-outline-success" type='submit'> ADD</button>
+
+                                <div class="col-sm-2 float-right" style="margin-bottom: 10px">
+                                    <button class="form-control btn-outline-success" type='submit'><?= ($course_id) ? "Edit" : "Add" ?></button>
                                 </div>
                             </div>
                         </div>
